d8e5049cd636ebfee3512d716ce2831ae8185df9
Neil Fordyce <N.Fordyce@dundee.ac.uk>
2014-01-10 18:42:40 +0000

    removing redundant polygon ops

--- scripts/PolyToRandPatch.py
+++ scripts/PolyToRandPatch.py
@@ -15,49 +15,42 @@ def rounded_bbox(polygon):
 	bbox = map(round, bbox)
 	bbox = map(int, bbox)
 	return bbox
-	
-def polygon_overlay(image, polygon, output_path):
-	overlay = image.copy()
-	draw = ImageDraw.Draw(overlay)
-	x, y = polygon.exterior.xy
-	x = map(int, x)
-	y = map(int, y)
-	draw.polygon(zip(x, y), fill='wheat')
-	blend_image = Image.blend(overlay, image, 0.5)
-	blend_image.save(output_path, quality=100)
-	
+
+#Convert a vector polygon into a raster image
 def polygon2image(polygon, output_size):
-	copy = Image.new('1', output_size, 'black')
-	draw = ImageDraw.Draw(copy)
-	x, y = polygon.exterior.xy
-	x = map(int, x)
-	y = map(int, y)
-	draw.polygon(zip(x, y), fill='white')
+	copy = Image.new('1', output_size, 'black')	#Blank canvas
+	draw = ImageDraw.Draw(copy)					#Draw the blank canvas
+	x, y = polygon.exterior.xy					#Get all the vertex coordinates
+	x = map(int, x)								
+	y = map(int, y)								
+	draw.polygon(zip(x, y), fill='white')		#Draw the polygon
 	return copy
 
 PATCH_SIZE = 200
+i = 0
 
 input_filename = sys.argv[1]
 number_of_samples = int(sys.argv[2])
 
-#read in all the points from lif file
+#read in all the points from lif annotation file
 file = open(input_filename, 'r')
-file = load(file)
+file = load(file)	#Deserialise the JSON
 
+#open up the electron micrograph image
 image = Image.open(file['imagePath'])
 polygons = file['shapes']
-i = 0
 
 #Make the list of points into a Shapely Polygon object
 for polygon in polygons:
-	polygon_e = Polygon(polygon['points'])
+	polygon = Polygon(polygon['points'])
 	i += 1
 	
-	bbox = rounded_bbox(polygon_e)
+	#Crop to the region of interest for efficiency
+	bbox = rounded_bbox(polygon)
 	golgi_crop = image.crop(bbox)
 	
 	#Shift the polygon to be over the golgi in golgi_crop image
-	polygon_e = translate(polygon_e, xoff=-bbox[0], yoff=-bbox[1])
+	polygon = translate(polygon, xoff=-bbox[0], yoff=-bbox[1])
 	
 	#Loop for each sample required
 	for j in range(number_of_samples):
@@ -68,53 +61,23 @@ for polygon in polygons:
 		while(not output):
 			#apply random rotation
 			rotation = randint(0, 359)
-			mask = polygon2image(polygon_e, golgi_crop.size)
+			mask = polygon2image(polygon, golgi_crop.size)
 			mask = mask.rotate(rotation, resample=Image.BICUBIC, expand=True)
-			
-			#image_center_point = map(lambda x: round(x/2), image.size)
-			#image_center_point = Point(image_center_point)
-			#polygon = rotate(polygon_e, rotation, origin='center')
-			#polygon = polygon_e
-			
-			#Get polygon bounding box
-			#bbox = rounded_bbox(polygon)
-		
-			#pick a point at random in the polygon's bbox
-			mask.size[0]
-			#x_range = (bbox[0], bbox[2])
-			#y_range = (bbox[1], bbox[3])
-			#x = randint(*x_range)
-			#y = randint(*y_range)
-			
-			x = randint(0, mask.size[0])
-			y = randint(0, mask.size[1])
-			
-			
-			point = Point(x, y)
-			
-			#and test if it is contained in the polygon
-			#if polygon.contains(point):
-			#if yes - plot a box around the point
+				
+			#pick a random box in the image
+			x = randint(0, mask.size[0] - PATCH_SIZE)
+			y = randint(0, mask.size[1] - PATCH_SIZE)
 			candidate_patch_coords = (x, y, x + PATCH_SIZE, y + PATCH_SIZE)
 			candidate_patch = box(*candidate_patch_coords)
-			#candidate_patch = rotate(candidate_patch, rotation, origin=image_center_point)
-			
+
+			#and then cut it out
 			crop_mask = mask.crop(candidate_patch_coords)
 			
-			#then test if the patch is contained in the polygon
-			#print crop_mask.histogram()
-			#if polygon.contains(candidate_patch):
+			#then test if the patch contains entirely golgi
 			if crop_mask.histogram()[-1] == PATCH_SIZE**2:
-			
 				#if yes - output the patch
-				print "center at %s" % point
-				print "created %s" % output_path
 				output = True
 				output_patch = golgi_crop.copy()
 				output_patch = output_patch.rotate(rotation, resample=Image.BICUBIC, expand=True)
-				
-				#polygon_overlay(output_patch, polygon, ".\output.bmp")
-				
-				#output_patch.save("%s_00_%s_%s" % (output_path, rotation, ".jpg"), format='JPEG', quality=100)
 				output_patch = output_patch.crop(candidate_patch_coords)
 				output_patch.save(output_path, format='JPEG', quality=100)
