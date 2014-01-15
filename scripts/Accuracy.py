beb69a9ff460c695e4173b53f4e48dee1aa2ae63
Neil Fordyce <N.Fordyce@dundee.ac.uk>
2014-01-15 15:19:11 +0000

    Streamlining - Detection in Accuracy script now

--- scripts/Accuracy.py
+++ scripts/Accuracy.py
@@ -1,23 +1,40 @@
+from cv2 import CascadeClassifier, imread
 from PolyToRandPatch import batch_open_deserialise, directory_paths
 from shapely.geometry import Polygon
 from shapely.geometry import box
 import sys
 
 try:
-	input_dir = sys.argv[1]
+	cascade_xml_path = sys.argv[1]
+	test_annotation_dir = sys.argv[2]
 except:
-	print "USAGE: python Accuracy input_dir"
+	print "USAGE: python Accuracy cascade_xml_path test_annotation_dir"
 	exit()
-	
-#Get the file paths to read from
-annotation_files = directory_paths(input_dir)
-#Open all the files and read the JSON
-annotation_files = batch_open_deserialise(annotation_files)
 
-for file in annotation_files:
+try:
+	#Open up the cascade classifier
+	cascade = CascadeClassifier(cascade_xml_path)
+except:
+	print "Error reading cascade xml"
+	exit()
+
+#Get the file paths to read from
+detector_output_files = directory_paths(test_annotation_dir)
+
+#Open all the files and read the JSON
+annotation_files = batch_open_deserialise([detector_output_files])
+
+#For each of the hand marked test files
+for annotation_file in annotation_files:
 	fp = 0
 	tp = 0
-	golgi_polygons = file['shapes']
+	
+	#Read the image corresponding to the annotation
+	img = imread(annotation_file['imagePath'])
+	classifier_output = cascade.detectMultiScale(img)	#Run the detector
+	
+	#Open all the files and read the JSON
+	golgi_polygons = annotation_file['shapes']
 
 	for golgi_polygon in golgi_polygons:
 		#Check the polygon is labelled as a golgi
@@ -27,10 +44,12 @@ for file in annotation_files:
 		#Make the list of points into a Shapely Polygon object
 		golgi_polygon = Polygon(polygon['points'])
 		
-		for detected_patch in classifier_output:
-			detected_patch = box(detected_patch)
+		for detected_rect in classifier_output:
+			#Convert the detected rectangle into a Shapely box 
+			detected_rect = box(*detected_rect)
 			
-			if golgi_polygon.contains(detected_patch):
+			#Test if the detected rectangle is contained in the golgi region
+			if golgi_polygon.contains(detected_rect):
 				tp += 1
 				
 	total_detections = len(classifier_output)
