32e09856b2989d6590e5dcfa730dd183255a5fbb
Neil Fordyce <N.Fordyce@dundee.ac.uk>
2014-04-26 00:06:36 +0100

    sampling latest

--- SamplingScripts/FourierHOG/GroundTruthMasks.py
+++ SamplingScripts/FourierHOG/GroundTruthMasks.py
@@ -7,63 +7,73 @@ import sys
 
 '''USAGE: python GroundTruthMasks input_dir output_dir'''
 
-#Check params are correct	
-try:
-	'''Get command line args'''
-	input_dir = sys.argv[1]
-	output_dir = sys.argv[2]
-except:
-	print "USAGE: python GroundTruthMasks input_dir output_dir"
-	exit()
-	
-#Output fill colours for different types of annotations
-annotation2fill = {	'golgi': 'white',
-					'ambiguous': 'blue'}
-
-#Get the file paths to read from
-annotation_files = directory_paths(input_dir)
-
-#Open all the files and read the JSON
-files = batch_open_deserialise(annotation_files)
-
-#For all the ground truths
-for file in files:
-	polygons = file['shapes']
-	image = Image.open(file['imagePath'])
-	
-	polygon_images = []
-	
-	#For each ground truth annotation
-	for polygon in polygons:	
-		polygon_label = polygon['label']
-	
-		#Make the list of points into a Shapely Polygon object
-		polygon = Polygon(polygon['points'])
-		
-		#Convert the polygon into an image
-		polygon_image = polygon2image(polygon, 
-										image.size, 
-										mode='RGB', 
-										polygon_fill=annotation2fill[polygon_label])
-		polygon_images.append(polygon_image)
-	
-	#Blend all of the polygon images together
-	output_image = polygon_images[0]
-
-	for polygon_image in polygon_images[1:]:
-		#Create a mask so we only change the part of the image being added
-		mtx = (1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0)
-		mask = polygon_image.convert("L", mtx)
-
-		output_image = Image.composite(polygon_image,
-										output_image, 
-										mask)	
-
-	#Construct the file name
-	filename, ext = splitext(file['imagePath'])
+def fix_path(filepath):
+	'''Sometime the annotation tool messes up the file path, pointing it to itself(i.e. the annotation file) instead of the electron micrograph image file. This should fix that'''
+	filename, ext = splitext(filepath)
 	filename = basename(filename)
-	output_path = join(output_dir, filename + ext)
+	filepath = join("C:/Users/Neil/SkyDrive/University/HonoursProject/annotated_images/golgi/", filename + ".jpg")	#Force the file ext. to .jpg.  
+	return filepath
 	
-	#Output
-	print output_path
-	output_image.save(output_path, format='JPEG', quality=100)
+if __name__ == '__main__':
+	#Check params are correct	
+	try:
+		'''Get command line args'''
+		input_dir = sys.argv[1]
+		output_dir = sys.argv[2]
+	except:
+		print "USAGE: python GroundTruthMasks input_dir output_dir"
+		exit()
+		
+	#Output fill colours for different types of annotations
+	annotation2fill = {	'golgi': 'white',
+						'ambiguous': 'blue'} #'white'}
+
+	#Get the file paths to read from
+	annotation_files = directory_paths(input_dir)
+
+	#Open all the files and read the JSON
+	files = batch_open_deserialise(annotation_files)
+
+	#For all the ground truths
+	for file in files:
+		polygons = file['shapes']
+		file['imagePath'] = fix_path(file['imagePath'])	#Force the file ext. to .jpg.  Sometimes it's stored wrong and needs this correction. 
+		print "opening " + file['imagePath']
+		image = Image.open(file['imagePath'])
+		
+		polygon_images = []
+		
+		#For each ground truth annotation
+		for polygon in polygons:	
+			polygon_label = polygon['label']
+		
+			#Make the list of points into a Shapely Polygon object
+			polygon = Polygon(polygon['points'])
+			
+			#Convert the polygon into an image
+			polygon_image = polygon2image(polygon, 
+											image.size, 
+											mode='RGB', 
+											polygon_fill=annotation2fill[polygon_label])
+			polygon_images.append(polygon_image)
+		
+		#Blend all of the polygon images together
+		output_image = polygon_images[0]
+
+		for polygon_image in polygon_images[1:]:
+			#Create a mask so we only change the part of the image being added
+			mtx = (1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0)
+			mask = polygon_image.convert("L", mtx)
+
+			output_image = Image.composite(polygon_image,
+											output_image, 
+											mask)	
+
+		#Construct the file name
+		filename, ext = splitext(file['imagePath'])
+		filename = basename(filename)
+		output_path = join(output_dir, filename + ext)
+		
+		#Output
+		print output_path
+		output_image.save(output_path, format='JPEG', quality=100)
--- SamplingScripts/ViolaJones/Accuracy.py
+++ SamplingScripts/ViolaJones/Accuracy.py
@@ -4,6 +4,7 @@ from shapely.geometry import Polygon
 from shapely.geometry import box
 from time import time
 import sys
+from GroundTruthMasks import fix_path
 
 try:
 	cascade_xml_path = sys.argv[1]
@@ -29,11 +30,12 @@ annotation_files = batch_open_deserialise(detector_output_files)
 #For each of the hand marked test files
 for annotation_file in annotation_files:	
 	#Read the image corresponding to the annotation
-	img = imread(annotation_file['imagePath'])
+	img = imread(fix_path(annotation_file['imagePath']))
 	print "detecting... "
 	classifier_output = cascade.detectMultiScale(img, 
-												minSize=(150, 150), 
-												maxSize=(250, 250))	#Run the detector
+												minNeighbors=3,
+												minSize=(175, 175), #Constrain scale within a range around training scale
+												maxSize=(225, 225))	#Run the detector
 	
 	total_detections = len(classifier_output)
 	fp = 0
@@ -44,25 +46,25 @@ for annotation_file in annotation_files:
 
 	for golgi_polygon in golgi_polygons:
 		#Make the list of points into a Shapely Polygon object
-		golgi_polygon = Polygon(golgi_polygon['points'])
+		golgi_polygon_shape = Polygon(golgi_polygon['points'])
 		
 		for detected_rect in classifier_output:
-			(x1, y1, x2, y2) = detected_rect
-			rectangle(img, (x1, y1),  (x2, y2), (0, 255, 0))
+			(x1, y1, w, h) = detected_rect
+			rectangle(img, (x1, y1),  (x1+w, y1+h), (0, 255, 0), thickness=3)
 			
 			#Convert the detected rectangle into a Shapely box 
-			detected_rect = box(*detected_rect)
+			detected_rect = box(x1, y1, x1+w, y1+h)
 			
 			#Test if the detected rectangle is contained in the golgi region
-			if golgi_polygon.contains(detected_rect):
+			if golgi_polygon_shape.intersects(detected_rect):
 				#Check the polygon is labelled as a golgi
-				if  golgi_polygon['label'] == 'golgi':
-					tp += 1	
-				else:
-					total_detections -= 1	#Ignore detections in ambiguous regions						
+				#if  golgi_polygon['label'] == 'golgi':
+				tp += 1	
+				#else:
+				#	total_detections -= 1	#Ignore detections in ambiguous regions		
 	
-	print "output"
 	millis = int(time() * 1000) 
+	print "output_%s.jpg" % millis
 	imwrite("output_%s.jpg" % millis, img)
 	fp = total_detections - tp
 	
